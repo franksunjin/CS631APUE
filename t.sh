85675fde2d26a1894c212d7f6eaff6bd0bd47943
franksunjin <frank@mac.home>
2014-09-17 10:19:33 -0400

    dd

--- t.sh
+++ t.sh
@@ -5,7 +5,9 @@ mkdir test_tcp/dir/
 mkdir  test_tcp/dir/filedir/
 echo "this is test" > test_tcp/1.txt
 echo "this is test" > this.txt
-./$1 test_tcp/1.txt 
+#wrong
+./$1 test_tcp/1.txt 	
+#wrong
 ./$1 test_tcp/1.txt test_tcp/2.txt sdd
 ./$1 test_tcp/1.txt test_tcp/2.txt
 ./$1 test_tcp/1.txt test_tcp/dir/2.txt
@@ -14,7 +16,11 @@ echo "this is test" > this.txt
 ./$1 test_tcp/1.txt test_tcp/dir
 ./$1 this.txt test_tcp/dir/
 ./$1 this.txt test_tcp/dir
+# No such file
 ./$1 test_tcp/1.txt test_tcp/disr/
+# No such file
 ./$1 test_tcp/1d.txt test_tcp/dir
-#rm -rf $PWD/test_tcp
+#wrong
+./$1 * test_tcp/dir
+rm -rf $PWD/test_tcp
 rm -rf this.txt
